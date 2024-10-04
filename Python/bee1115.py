69c9ed1dbf99b2ce741bc11e9aaf033814b15d71
Lucas Henrique Haboski da Silva <lucas.hhaboski@gmail.com>
2024-10-04 13:13:16 -0300

    Create bee1115.py

---
+++ Python/bee1115.py
@@ -0,0 +1,15 @@
+# Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+x = 1
+y = 1
+
+while x != 0 and y != 0:
+    x, y = map(int, input().split())
+
+    if x > 0 and y > 0:
+        print('primeiro')
+    elif x < 0 and y > 0:
+        print('segundo')
+    elif x < 0 and y < 0:
+        print('terceiro')
+    elif x > 0 and y < 0:
+        print('quarto')
