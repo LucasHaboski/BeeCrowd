0698b57e1ee4d14cfdc4857385edfb39b92e6218
Lucas Henrique Haboski <lucas.hhaboski@gmail.com>
2025-12-03 15:42:59 -0300

    nova resolução bee1067

--- Python/bee1067.py
+++ Python/bee1067.py
@@ -1,13 +1,20 @@
 num = int(input())
-numLimite = int(input())
 
-if num > numLimite:
-    num, numLimite = numLimite, num
-
-soma = 0
-
-for i in range (num + 1, numLimite):
+for i in range(0,num+1):
     if i % 2 != 0:
-        soma = soma + i
+        print(i)
+
+#1067 antigo
+#num = int(input())
+#numLimite = int(input())
+
+#if num > numLimite:
+#    num, numLimite = numLimite, num
+
+#soma = 0
+
+#for i in range (num + 1, numLimite):
+#    if i % 2 != 0:
+#        soma = soma + i
     
-print(soma)
+#print(soma)
